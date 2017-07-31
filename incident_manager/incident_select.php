cd54288bfe930406ab3c3edc6aae355aac11e809
Fred Scott <fscott@southeast.edu>
2017-07-31 16:06:14 -0500

    added starter files

---
+++ incident_manager/incident_select.php
@@ -0,0 +1,10 @@
+<?php include '../view/header.php'; ?>
+<main>
+
+    <h2>Create the Incident Select Page and CHANGE THIS HEADLINE</h2>
+    <p>your list goes here?</p>
+    
+    
+    <p><a href="?action=display_customer_get">Make an Incident</a></p>
+</main>
+<?php include '../view/footer.php'; ?>
---
+++ view/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+
+<!-- the head section -->
+<head>
+    <title>SportsPro Technical Support</title>
+    <link rel="stylesheet" type="text/css"
+	
+          href="/<?php echo array_pop(explode("\\", dirname(__DIR__))) ?>/main.css">
+			<!--- THIS PATH WILL BREAK ON LINUX/OSX 
+		replace "\\" with "//"
+		-->
+		  </head>
+
+<!-- the body section -->
+<body>
+<header>
+    <h1>SportsPro Technical Support</h1>
+    <p>Sports management software for the sports enthusiast</p>
+    <nav>
+        <ul>
+            <li><a href="/<?php echo array_pop(explode("\\", dirname(__DIR__))) ?>">Home</a></li>
+        </ul>
+    </nav>
+</header>
